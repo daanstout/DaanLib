9266dbc4c7c760b4ce920a6b171131a3b5b71644
Daan Stout <formaestroke@gmail.com>
2019-05-04 10:51:17 +0100

    added a class that allows you to drag borderless windows

---
+++ DaanLib/DaanLib/WinFormHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaanLib {
+    /// <summary>
+    /// Some ease of use functions for WinForms
+    /// </summary>
+    public static class WinFormHelper {
+        /// <summary>
+        /// 
+        /// </summary>
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        /// <summary>
+        /// 
+        /// </summary>
+        public const int HT_CAPTION = 0x2;
+
+        /// <summary>
+        /// Sends a message to the system
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="Msg"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        /// <summary>
+        /// Releases the capture of the window
+        /// </summary>
+        /// <returns></returns>
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        /// <summary>
+        /// Allows the user to drag the window
+        /// </summary>
+        /// <param name="handle"></param>
+        public static void HandleWindowDrag(IntPtr handle) {
+            ReleaseCapture();
+            SendMessage(handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+        }
+    }
+}
